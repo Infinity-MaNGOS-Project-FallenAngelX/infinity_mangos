272a1ad38cefe120897f1daaa49ba336daf37e99
GMHAZE <iceis2@hotmail.com>
2010-11-13 01:57:17 -0500

    {X:2} Infinity Project Set up & Fixed Warnings with Current AntiCheat & Fixed Some Tabs

--- src/mangosd/Main.cpp
+++ src/mangosd/Main.cpp
@@ -151,18 +151,18 @@ extern int main(int argc, char **argv)
     sLog.outString( "%s [world-daemon]", _FULLVERSION(REVISION_DATE,REVISION_TIME,REVISION_NR,REVISION_ID) );
     sLog.outString( "<Ctrl-C> to stop.\n\n" );
 
-     sLog.outTitle( "               P   R   O   J   E   C   T __ TheFallen_WoW __                  ");	
-    sLog.outTitle( "              A Special Made Core For & By FallenAngelX                        ");	
-    sLog.outTitle( ":::::::::      :::     :::        :::     ::::::::   :::::::::                 ");
-	sLog.outTitle( ":+:          :+: :+:   :+:        :+:     :+:        :+:    :+:                ");
-	sLog.outTitle( "+:+         +:+   +:+  +:+        +:+     +:+        +:+     +:+               ");
-    sLog.outTitle( "+:+::::::   #++:++#++: +#+        +#+     +#++:++#   +#+     +#+               ");
-    sLog.outTitle( "+:+        +#+     +#+ +#+        +#+     +#+        +#+     +#+               ");
-    sLog.outTitle( "#+#        #+#     #+# #+#        #+#     #+#        #+#     #+#               ");
-    sLog.outTitle( "###        ###     ### #######    ####### ########   ###     ###               ");
-    sLog.outTitle( "Special Made Core By and for FallenAngelX 3.3.5 Personal Core                  ");	
-	sLog.outTitle( "Github.com/fallenangelX/(Retired Developer From Hellscream & Dark_Ice)"         );
-	sLog.outTitle( "Project The Fallen World of Warcraft Server 3.3.5                              ");
+    sLog.outTitle( "               P   R   O   J   E   C   T __ The Infinity Project __           ");	
+    sLog.outTitle( "              A Special Made Core For & By FallenAngelX           I            ");	
+    sLog.outTitle( ":::::::::      :::     :::        :::     ::::::::   :::::::::     N   P       ");
+    sLog.outTitle( ":+:          :+: :+:   :+:        :+:     :+:        :+:    :+:     F   R      ");
+    sLog.outTitle( "+:+         +:+   +:+  +:+        +:+     +:+        +:+     +:+     I   O     ");
+    sLog.outTitle( "+:+::::::   #++:++#++: +#+        +#+     +#++:++#   +#+     +#+      N   J    ");
+    sLog.outTitle( "+:+        +#+     +#+ +#+        +#+     +#+        +#+     +#+       I   E   ");
+    sLog.outTitle( "#+#        #+#     #+# #+#        #+#     #+#        #+#     #+#        T   C  ");
+    sLog.outTitle( "###        ###     ### #######    ####### ########   ###     ###         Y   T ");
+    sLog.outTitle( "Special Made Core By and for The Players                                       ");	
+    sLog.outTitle( "Github.com/fallenangelX/(Retired Developer From Hellscream & Dark_Ice)         ");
+    sLog.outTitle( "Project The InFinity Project  Core Developed orginally for TheFallen_Server    ");
 
     sLog.outString("Using configuration file %s.", cfg_file);
 
