4b08989595838eff661704b1d61c80bbae6a8845
GMHAZE <iceis2@hotmail.com>
2010-11-15 09:01:19 -0500

    typo

--- src/game/Spell.cpp
+++ src/game/Spell.cpp
@@ -3030,7 +3030,6 @@ void Spell::cast(bool skipCheck)
             if (m_spellInfo->Id == 64382)
                 AddTriggeredSpell(64380);                    // Shattering Throw
             // Shield Slam
-            if ((m_spellInfo->SpellFamilyFlags & UI64LIT(0x0000020000000000)) && m_spellInfo->Category==1209)
             else if ((m_spellInfo->SpellFamilyFlags & UI64LIT(0x0000020000000000)) && m_spellInfo->Category==1209)
                 if (m_caster->HasAura(58375))               // Glyph of Blocking
                     AddTriggeredSpell(58374);               // Glyph of Blocking
