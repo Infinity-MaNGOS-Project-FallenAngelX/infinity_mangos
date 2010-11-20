5b9b9ea4c2da18602a7c31ca1f77103d8c30329f
GMHAZE <iceis2@hotmail.com>
2010-11-20 17:06:45 -0500

    SQL

--- Infinity_FullDB/DBs_Updates/07_MaNGOS_Start_up_errors_3.sql
+++ Infinity_FullDB/DBs_Updates/07_MaNGOS_Start_up_errors_3.sql
@@ -1,16 +1,15 @@
 UPDATE creature_ai_scripts SET action1_param1 = 59246 WHERE id = 2669101;
 UPDATE creature_ai_scripts SET action1_param1 = 59246 WHERE id = 2669101;
-UPDATE `creature_ai_texts` SET `content_default`='Tharon ja sees all! The work of mortals shall not end the eternal dynasty!' WHERE `entry`='-688';
-UPDATE `creature_ai_texts` SET `content_default`='Im... impossible! Tharon ja is eternal! Tharon ja... is...' WHERE `entry`='-689';
-UPDATE `creature_ai_texts` SET `content_default`='As Tharon ja predicted.' WHERE `entry`='-690';
-UPDATE `creature_ai_texts` SET `content_default`='As it was written.' WHERE `entry`='-691';
-UPDATE `creature_ai_texts` SET `content_default`='Your flesh serves Tharon ja now!' WHERE `entry`='-692';
-UPDATE `creature_ai_texts` SET `content_default`='Tharon ja has a use for your mortal shell!' WHERE `entry`='-693';
 DELETE FROM `spell_proc_event` WHERE (`entry`='48495'); 
 DELETE FROM `spell_proc_event` WHERE (`entry`='48494'); 
 DELETE FROM `spell_proc_event` WHERE (`entry`='44545');
 
-
+INSERT INTO creature_ai_texts (`entry`, `content_default`, `sound`, `type`, `language`, `comment`) VALUES (-688, 'Tharon ja sees all! The work of mortals shall not end the eternal dynasty!', '0', '0', '0', 'insert comment here (optional)');
+INSERT INTO creature_ai_texts (`entry`, `content_default`, `sound`, `type`, `language`, `comment`) VALUES (-689, 'Im... impossible! Tharon ja is eternal! Tharon ja... is...', '0', '0', '0', 'insert comment here (optional)');
+INSERT INTO creature_ai_texts (`entry`, `content_default`, `sound`, `type`, `language`, `comment`) VALUES (-690, 'As Tharon ja predicted.', '0', '0', '0', 'insert comment here (optional)');
+INSERT INTO creature_ai_texts (`entry`, `content_default`, `sound`, `type`, `language`, `comment`) VALUES (-691, 'As it was written.', '0', '0', '0', 'insert comment here (optional)');
+INSERT INTO creature_ai_texts (`entry`, `content_default`, `sound`, `type`, `language`, `comment`) VALUES (-692, 'Your flesh serves Tharon ja now!', '0', '0', '0', 'insert comment here (optional)');
+INSERT INTO creature_ai_texts (`entry`, `content_default`, `sound`, `type`, `language`, `comment`) VALUES (-693, 'Tharon ja has a use for your mortal shell!', '0', '0', '0', 'insert comment here (optional)');
 
 --- FallenAngelX
 ALTER TABLE db_version_Infinity_Update CHANGE COLUMN r06 r07 bit;
