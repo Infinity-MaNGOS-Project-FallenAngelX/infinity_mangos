94f026870ce1234c1e9290ddf4d4cc5b223b0d28
GMHAZE <iceis2@hotmail.com>
2010-11-20 22:56:37 -0500

    DB & R&D to there own Repo

--- Infinity_FullDB/00_MaNGOS.sql
+++
@@ -1,7 +0,0 @@
-
---- FallenAngelX
-ALTER TABLE db_version_Infinity_Update CHANGE COLUMN r00 r01 bit;
-REPLACE INTO `db_version_Infinity_Update` (`version`) VALUES ('r01');
-
-UPDATE db_version SET `cache_id`= 'r01';
-UPDATE db_version SET `version`= 'YTDB572_Infinity_Update_r01';
