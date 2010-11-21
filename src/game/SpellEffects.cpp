03c135618371e48682c3c6c988eeedbfd686718c
GMHAZE <iceis2@hotmail.com>
2010-11-20 22:35:23 -0500

    CFs #5 & #6  ty to Kero99

--- src/game/SpellEffects.cpp
+++ src/game/SpellEffects.cpp
@@ -7778,7 +7778,7 @@ void Spell::EffectScriptEffect(SpellEffectIndex eff_idx)
         return;
 
     DEBUG_FILTER_LOG(LOG_FILTER_SPELL_CAST, "Spell ScriptStart spellid %u in EffectScriptEffect ", m_spellInfo->Id);
-    if (m_caster->IsInWorld())
+    if (m_caster->GetMapSafe() && m_caster->IsInWorld())
         m_caster->GetMap()->ScriptsStart(sSpellScripts, m_spellInfo->Id, m_caster, unitTarget);
 }
 
