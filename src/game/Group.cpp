45243f9751bdd2f10c0cc3913aacf1461f63f3a4
GMHAZE <iceis2@hotmail.com>
2010-11-25 21:35:47 -0500

    updated w_rsa & mangos 11_25_2010

--- src/game/Group.cpp
+++ src/game/Group.cpp
@@ -1603,7 +1603,7 @@ GroupJoinBattlegroundResult Group::CanJoinBattleGroundQueue(BattleGround const* 
         return ERR_BATTLEGROUND_JOIN_FAILED;
 
     uint32 arenaTeamId = reference->GetArenaTeamId(arenaSlot);
-    uint32 team = reference->GetTeam();
+    Team team = reference->GetTeam();
 
     uint32 allowedPlayerCount = 0;
 
